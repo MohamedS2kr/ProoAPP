b32c2db97755b2a0dd254446b860fb3f639e7739
Mohamed S2kr <75217623+MohamedS2kr@users.noreply.github.com>
2024-09-15 19:45:15 +0300

    ignoreController Error

--- Proo.APIs/Controllers/ErrorsController.cs
+++ Proo.APIs/Controllers/ErrorsController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper.Configuration.Annotations;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Proo.APIs.Errors;
 
 namespace Proo.APIs.Controllers
 {
-  
+    [ApiExplorerSettings(IgnoreApi =true)]
     public class ErrorsController : BaseApiController
     {
 
