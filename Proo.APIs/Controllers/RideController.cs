0272e3ea492d841b82084b4dc752b090fbcb7929
Youssef Samir <youssefmansour3076y@gmail.com>
2024-09-29 00:53:48 +0300

    add accept bid service and endpoint

--- Proo.APIs/Controllers/RideController.cs
+++ Proo.APIs/Controllers/RideController.cs
@@ -14,6 +14,7 @@ using Proo.Core.Contract.Driver_Contract;
 using Proo.Core.Contract.Passenger_Contract;
 using Proo.Core.Contract.RideService_Contract;
 using Proo.Core.Entities;
+using Proo.Core.Specifications.BidSpecifications;
 using Proo.Core.Specifications.DriverSpecifiactions;
 using Proo.Infrastructer.Data.Context;
 using Proo.Service._RideService;
@@ -211,6 +212,7 @@ namespace Proo.APIs.Controllers
             // Step 6: create Bid entity
             var bid = _mapper.Map<Bid>(bidDto);
             bid.CreatedAt = DateTime.Now;
+            bid.BidStatus = BidStatus.Pendding;
 
             _unitOfWork.Repositoy<Bid>().Add(bid);
             var addBid = await _unitOfWork.CompleteAsync();
@@ -240,6 +242,99 @@ namespace Proo.APIs.Controllers
                 }
             });  // TODO
 
+        }
+
+        [Authorize(Roles =Passenger)]
+        [HttpPost("accept-bid")]
+        public async Task<ActionResult> AcceptBid([FromBody]AcceptBidRequestDto acceptBidDto)
+        {
+            var phoneNumber = User.FindFirstValue(ClaimTypes.MobilePhone);
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.PhoneNumber == phoneNumber);
+
+            var passenger = await _passengerRepo.GetByUserId(user.Id);
+            if (passenger is null) return BadRequest(new ApiResponse(400, "Passenger is not exist"));
+            // step 1 : Get the bid and validate it
+            var spec = new BidWithRideRequestSpecifications(acceptBidDto.BidId);
+            var bid = await _unitOfWork.Repositoy<Bid>().GetByIdWithSpecAsync(spec);
+            if (bid is null) return BadRequest(new ApiResponse(400, "The Bid is not exist."));
+
+            // validate bid status 
+            if (bid.BidStatus != BidStatus.Pendding)
+                return BadRequest(new ApiResponse(400, "The selected bid is not lavailable."));
+
+            // trip request is invalid/expired if trip request is older than 1 minute TODO
+            var onminutesAgo = DateTime.Now.AddMinutes(-1);
+            if (bid.Ride.LastModifiedAt < onminutesAgo) return BadRequest(new ApiResponse(400, "Ride Reuest is expired."));
+
+            //// step 2 : Ensure that the driver exists
+            //var driver = await _unitOfWork.Repositoy<Driver>().GetDriverOrPassengerByIdAsync(DriverId);
+            //if (driver is null) return BadRequest(new ApiResponse(400, "The Driver is not exist."));
+
+            //// Step 3: check driver has ongoing trip requests
+            //var driveronGoingRideReuest = await _unitOfWork.RideRequestRepository.GetActiveTripRequestForDriver(DriverId);
+            //if (driveronGoingRideReuest is not null) return BadRequest(new ApiResponse(400, "Driver has an ongoing Ride request."));
+
+            //// Step 4: check driver has ongoing trips
+            //var rides = await _unitOfWork.RideRepository.GetActiveTripForDriver(DriverId);
+            //if (rides is not null) return BadRequest(new ApiResponse(400, "Driver has an ongoing trips"));
+
+            // check passenger has ongoing ride request 
+            var passengerOnGoingRideRequest = await _unitOfWork.RideRequestRepository.GetActiveTripRequestForPassenger(passenger.Id);
+            if(passengerOnGoingRideRequest is not null) return BadRequest(new ApiResponse(400, "Passenger has an ongoing Ride request."));
+
+            // check passenger has ongoing trips 
+            var rides = await _unitOfWork.RideRepository.GetActiveTripForPassenger(passenger.Id);
+            if (rides is not null) return BadRequest(new ApiResponse(400, "Passenger has an ongoing trips."));
+
+            // step 5: Validate that the ride request is still open
+            var rideReuest = bid.Ride;
+            if (rideReuest is null || rideReuest.Status != RideRequestStatus.Requested)
+                return BadRequest(new ApiResponse(400, "Ride request is not available."));
+
+
+            // step 6 : Update Ride Request and Bid Status
+            bid.BidStatus = BidStatus.Accepted;
+            // bid --> Accepted at 
+
+            rideReuest.Status = RideRequestStatus.CUSTOMER_ACCEPTED;
+            rideReuest.DriverId = bid.DriverId;
+
+            _unitOfWork.Repositoy<RideRequests>().Update(rideReuest);
+
+
+            //step 7 : Update other bids to rejected
+            var bidRepo = _unitOfWork.Repositoy<Bid>();
+            var BidsSpec = new BidWithOtherBidsForDriverSpecifications(rideReuest.Id, bid.Id);
+            var otherBids = await bidRepo.GetAllWithSpecAsync(BidsSpec);
+
+            foreach (var other in otherBids)
+            {
+                other.BidStatus = BidStatus.Rejected;
+                bidRepo.Update(other);
+            }
+
+            
+
+            //step 8 : Update Driver Status to Unavailable
+            var driverRepo = _unitOfWork.Repositoy<Driver>();
+            var driver = await driverRepo.GetDriverOrPassengerByIdAsync(bid.DriverId);
+            if (driver is null)
+                return BadRequest(new ApiResponse(400, "Driver is not exist."));
+
+            driver.Status = DriverStatus.InRide;
+            driverRepo.Update(driver);
+
+            // Save changes
+            var count = await _unitOfWork.CompleteAsync();
+            if (count <= 0) return BadRequest(new ApiResponse(400, "The error when save change in database"));
+
+            return Ok(new
+            {
+                Message = "Bid accepted successfully.",
+                BidId = bid.Id,
+                RideRequestId = rideReuest.Id,
+                DriverId = bid.DriverId
+            });
         }
 
 
@@ -326,7 +421,7 @@ namespace Proo.APIs.Controllers
         //    return Ok(response);
         //}
 
-        
+
 
 
         [HttpPost("calc_price_time_destance")]
---
+++ Proo.Core/Specifications/BidSpecifications/BidWithRideRequestSpecifications.cs
@@ -0,0 +1,18 @@
+﻿using Proo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proo.Core.Specifications.BidSpecifications
+{
+    public class BidWithRideRequestSpecifications : BaseSpecifications<Bid>
+    {
+        public BidWithRideRequestSpecifications(int bidId)
+            :base(b => b.Id == bidId)
+        {
+            Includes.Add(b => b.Ride);
+        }
+    }
+}
